224d07b096ab23bf3caed1322303436afcb65a88
phucdev66 <phucledev66@gmail.com>
2025-11-28 07:08:05 +0700

    Add email confirmation feature for successful bookings and update application configuration

--- src/main/java/com/example/spring3/Spring3Application.java
+++ src/main/java/com/example/spring3/Spring3Application.java
@@ -2,8 +2,10 @@ package com.example.spring3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Spring3Application {
 
 	public static void main(String[] args) {
